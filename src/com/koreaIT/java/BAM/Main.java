4b50702124f6287d141646d5c13792471ad7e491
codingpgw <pgw20000@gmail.com>
2023-05-17 10:40:33 +0900

    게시판 작성 게시물 목록 기능 구현

--- src/com/koreaIT/java/BAM/Main.java
+++ src/com/koreaIT/java/BAM/Main.java
@@ -7,53 +7,63 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("== 프로그램 시작 ==");
-		
 		Scanner sc = new Scanner(System.in);
-		
 		int lastArticleId = 0;
 		
 		List<Article> articles = new ArrayList<>();
 		
 		while(true) {
-			System.out.printf("명령어) ");
+			System.out.printf("명령어) ");	
 			String cmd = sc.nextLine().trim();
 			
-			if(cmd.length() == 0){ 
+			if(cmd.length() == 0) {
 				System.out.println("명령어를 입력해주세요.");
 				continue;
 			}
 			
 			if(cmd.equals("exit")) {
 				break;
-			}else if(cmd.equals("article list")) {
-				System.out.println("게시글이 없습니다.");
-			}else if(cmd.equals("article write")) {
-				int id = lastArticleId + 1;
-				System.out.printf("제목 :");
+			} 
+			
+			if(cmd.equals("article list")) {
+				
+				if(articles.size() == 0) {
+					System.out.println("게시글이 없습니다.");				
+					continue;
+				}
+				System.out.println("번호	|	제목	");
+
+				for(int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+					System.out.printf("%d	|     %s	\n", article.id, article.title);
+				}
+//			split, startWith
+			} else if(cmd.equals("article write")) {
+				System.out.printf("제목 : ");
 				String title = sc.nextLine();
-				System.out.printf("내용 :");
+				System.out.printf("내용 : ");
 				String body = sc.nextLine();
+				int id = lastArticleId + 1;
+				lastArticleId = id;
 				
 				Article article = new Article(id, title, body);
+						
+				articles.add(article);
 				
-				System.out.printf("%d번 글이 생성되었습니다.\n", id);
-				
-				lastArticleId++;
-			}else {
+				System.out.printf("%d번글이 생성되었습니다.\n", id);
+			} else {
 				System.out.println("존재하지 않는 명령어입니다.");
 			}
 			
-			
 		}
-		
-		System.out.println("== 프로그램 끝 ==");
 		
 		sc.close();
 		
+		System.out.println("== 프로그램 끝 ==");
 	}
 }
 
-class Article {
+class Article{
 	int id;
 	String title;
 	String body;
